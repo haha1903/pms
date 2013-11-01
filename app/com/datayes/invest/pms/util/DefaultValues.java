e3490b0091116c9dbdaa085a159a3c1b54577a84
tao.tao <tao.tao@localhost.localdomain>
2013-11-01 01:52:59 +0000

    Adding DefaultValues

---
+++ app/com/datayes/invest/pms/util/DefaultValues.java
@@ -0,0 +1,7 @@
+package com.datayes.invest.pms.util;
+
+public class DefaultValues {
+
+    private DefaultValues() {
+    }
+}
---
+++ test/com/datayes/invest/pms/service/marketdata/impl/MarketDataServiceMockImpl.java
@@ -0,0 +1,38 @@
+package com.datayes.invest.pms.service.marketdata.impl;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.joda.time.LocalDate;
+
+import com.datayes.invest.pms.service.marketdata.MarketData;
+import com.datayes.invest.pms.service.marketdata.MarketDataService;
+
+public class MarketDataServiceMockImpl implements MarketDataService {
+    
+    private Map<Long, MarketData> equityMarketDataMap = Collections.emptyMap();
+    
+    private Map<Long, MarketData> futureMarketDataMap = Collections.emptyMap();
+
+    @Override
+    public Map<Long, MarketData> getEquityMarketData(Set<Long> securityIds, LocalDate asOfDate) {
+        return equityMarketDataMap;
+    }
+
+    @Override
+    public Map<Long, MarketData> getFutureMarketData(Set<Long> securityIds, LocalDate asOfDate) {
+        return futureMarketDataMap;
+    }
+    
+    public void setEquityMarketDataMap(Map<Long, MarketData> map) {
+        Map<Long, MarketData> newMap = new HashMap<>(map);
+        this.equityMarketDataMap = Collections.unmodifiableMap(newMap);
+    }
+    
+    public void setFutureMarketDataMap(Map<Long, MarketData> map) {
+        Map<Long, MarketData> newMap = new HashMap<>(map);
+        this.futureMarketDataMap = Collections.unmodifiableMap(newMap);
+    }
+}
