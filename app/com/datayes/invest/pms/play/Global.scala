b4569ed4c223d0bfd4dcd44e728a602fdef1ce0e
tao.tao <tao.tao@localhost.localdomain>
2013-11-11 11:01:46 +0000

    Fix the problem that net worth is not calculated correctly on account open date.

--- app/com/datayes/invest/pms/play/Global.scala
+++ app/com/datayes/invest/pms/play/Global.scala
@@ -33,9 +33,11 @@ object Global extends WithFilters(new LoggingFilter) with Logging {
   }
   
   private def initializeSystem() {
-    val scheduler = injector.getInstance(classOf[SystemScheduler])
-    val thread = new Thread(scheduler)
-    thread.start()
+    if (runningMode != RunningMode.NO_DAEMON) {
+      val scheduler = injector.getInstance(classOf[SystemScheduler])
+      val thread = new Thread(scheduler)
+      thread.start()
+    }
   }
   
   private def createInjectors(): Unit = {
