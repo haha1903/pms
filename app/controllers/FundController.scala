1d9e43f9a3a4a6c2be4269606ad8b3dd0a9c14c6
tao.tao <tao.tao@localhost.localdomain>
2013-11-14 09:30:20 +0000

    Move get/set dashboard layout from Dashboard2Controller to FundController

--- app/controllers/FundController.scala
+++ app/controllers/FundController.scala
@@ -8,16 +8,40 @@ import com.datayes.invest.pms.web.model.models.ModelWrites._
 import play.api.libs.json.Json
 import play.pms.{PmsAction, PmsController}
 import org.joda.time.LocalDate
+import com.datayes.invest.pms.userpref.UserPref
 
 
 class FundController extends PmsController with Logging {
+  
   @Inject
   private var fundService: FundService = null
+  
+  @Inject
+  private var userPref: UserPref = null
 
   def getSummary() = PmsAction { implicit req =>
     val accountId: Long = param("accountId")
     val asOfDate: LocalDate = paramAsOfDateOrToday
     val summary = fundService.getSummary(accountId, asOfDate)
     Json.toJson(summary)
+  }
+  
+  def getDashboardLayout = PmsAction { implicit req =>
+    val config = userPref.getDashboardLayoutConfig()
+    val json = Json.obj("config" -> config)
+    json
+  }
+  
+  def setDashboardLayout = PmsAction { implicit req =>
+    val config: String = param("config")
+    
+    val (result, message) = try {
+      userPref.setDashboardLayoutConfig(config)
+      (true, "layout config has bean saved")
+    } catch {
+      case e: Throwable => (false, e.getMessage)
+    }
+    val json = Json.obj("success" -> result, "message" -> message)
+    json
   }
 }
