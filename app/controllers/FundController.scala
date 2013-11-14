3687cf9fc79bb169214ddceac5fc90fbbfbb60dc
baoan.feng <baoan.feng@localhost.localdomain>
2013-11-14 09:21:29 +0000

    bug fix for fund/summary

--- app/controllers/FundController.scala
+++ app/controllers/FundController.scala
@@ -7,14 +7,16 @@ import com.datayes.invest.pms.web.service.FundService
 import com.datayes.invest.pms.web.model.models.ModelWrites._
 import play.api.libs.json.Json
 import play.pms.{PmsAction, PmsController}
+import org.joda.time.LocalDate
 
 
 class FundController extends PmsController with Logging {
   @Inject
   private var fundService: FundService = null
 
-  def getSummary(accountId: Long) = PmsAction { implicit req =>
-    val asOfDate = paramAsOfDateOrToday
+  def getSummary() = PmsAction { implicit req =>
+    val accountId: Long = param("accountId")
+    val asOfDate: LocalDate = paramAsOfDateOrToday
     val summary = fundService.getSummary(accountId, asOfDate)
     Json.toJson(summary)
   }
