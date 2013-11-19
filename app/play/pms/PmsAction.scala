9f9e6441186ccd7ad7f9f7b4e1d172e003830f0a
tao.tao <tao.tao@localhost.localdomain>
2013-11-19 06:37:20 +0000

    PMS-294 Add jsonp for responses, which is missing in the implementation.

--- app/play/pms/PmsAction.scala
+++ app/play/pms/PmsAction.scala
@@ -16,6 +16,7 @@ import play.api.mvc.Result
 import com.datayes.invest.pms.web.sso.SamlActionTrait
 import com.datayes.invest.pms.config.Config
 import com.datayes.invest.pms.web.sso.AuthAction
+import play.api.libs.Jsonp
 
 
 trait PmsActionBuilder extends Logging {
@@ -31,33 +32,36 @@ trait PmsActionBuilder extends Logging {
   
   
   def apply[A](bodyParser: BodyParser[A])(block: Request[A] => PmsResult): Action[A] = new AuthAction[A](bodyParser)({ req =>
-
+    val callback = req.getQueryString("callback")
     try {
       val result = block(req)
       result match {
-        case res: PmsJsResult => respond(res)
-        case res: PmsStrResult => respond(res)
+        case res: PmsJsResult => respond(res, callback)
+        case res: PmsStrResult => respond(res, callback)
       }
         
     } catch {
       case e: NotImplementedError => throw new RuntimeException(e)
       case e: LinkageError => throw new RuntimeException(e)
-      case e: ClientException => exception(e)
-      case th: Throwable => exception(th)
+      case e: ClientException => exception(e, callback)
+      case th: Throwable => exception(th, callback)
     }    
   })
 
-  private def respond(res: PmsJsResult) = {
+  private def respond(res: PmsJsResult, callback: Option[String]) = {
     val respjson = if (useDyResponse) {
       val dyresp = DYJsResponse(Status.OK, "", res.json)
       Json.toJson(dyresp)
     } else {
       res.json
     }
-    Ok(respjson)
+    callback match {
+      case Some(c) => Ok(Jsonp(c, respjson))
+      case None => Ok(respjson)
+    }
   }
     
-  private def respond(res: PmsStrResult) = {
+  private def respond(res: PmsStrResult, callback: Option[String]) = {
     val content = res.content
     val resp = if (useDyResponse) {
       val dyresp = DYStrResponse.create(Status.OK, "", content)
@@ -65,25 +69,37 @@ trait PmsActionBuilder extends Logging {
     } else {
       content
     }
-    Ok(resp).as("application/json; charset=utf-8")
+    val resp2 = callback match {
+      case Some(c) => jsonp(resp, c)
+      case None => resp
+    }
+    Ok(resp2).as("application/json; charset=utf-8")
   }
-    
-  private def exception(e: ClientException) = {
+  
+  private def jsonp(data: String, callback: String): String = callback + "(" + data + ");"
+  
+  private def exception(e: ClientException, callback: Option[String]) = {
     if (useDyResponse) {
       val dyresp = DYJsResponse(Status.BAD_REQUEST, e.getMessage(), JsString(e.getStackTraceString))
       val respjson = Json.toJson(dyresp)
-      BadRequest(respjson)
+      callback match {
+        case Some(c) => BadRequest(Jsonp(c, respjson))
+        case None => BadRequest(respjson)
+      }
     } else {
       throw e
     }
   }
     
-  private def exception(e: Throwable) = {
+  private def exception(e: Throwable, callback: Option[String]) = {
     logger.warn("Internal server error: {}", e.getMessage(), e)
     if (useDyResponse) {
       val dyresp = DYJsResponse(Status.INTERNAL_SERVER_ERROR, e.getMessage, JsString(e.getStackTraceString))
       val respjson = Json.toJson(dyresp)
-      InternalServerError(respjson)
+      callback match {
+        case Some(c) => InternalServerError(Jsonp(c, respjson))
+        case None => InternalServerError(respjson)
+      }
     } else {
       throw e
     }
