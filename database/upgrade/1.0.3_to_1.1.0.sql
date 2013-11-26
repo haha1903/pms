f6c7e07e2faec4813bc607e5860662afeb35b8e7
baoan.feng <baoan.feng@localhost.localdomain>
2013-11-26 06:27:02 +0000

    JIRA:PMS-293 delete wrong content at last commit

--- database/upgrade/1.0.3_to_1.1.0.sql
+++ database/upgrade/1.0.3_to_1.1.0.sql
@@ -17,5 +17,3 @@ create table MARKET_DATA
  * 2. Add foreign keys
  */
 alter table POSITION_YIELD add TRADE_EARN_CAMT decimal(31,11) default 0 not null comment "交易盈亏";
-alter table POSITION_YIELD add constraint fk_PositionId foreign key (POSITION_ID) references POSITION(POSITION_ID);
-alter table POSITION_YIELD add constraint fk_AccountId foreign key (ACCOUNT_ID) references ACCOUNT(ACCOUNT_ID);
